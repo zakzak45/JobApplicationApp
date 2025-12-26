b79dd9b19ae7ea6cdbee961c0149386cbf8987ad
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2025-12-26 04:31:10 +0200

    Initial commit

---
+++ server/Server3/Server4/src/main/java/server4/example/Server4/Server4Application.java
@@ -0,0 +1,13 @@
+package server4.example.Server4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Server4Application.class, args);
+	}
+
+}
---
+++ server/Server3/Server4/src/test/java/server4/example/Server4/Server4ApplicationTests.java
@@ -0,0 +1,13 @@
+package server4.example.Server4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
